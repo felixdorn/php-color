cbc7499f6c41b6bcbbdd36c49dd9a4dec4dab70b
Félix Dorn <github@felixdorn.fr>
2024-05-26 14:05:02 +0200

    use the same php for php and composer

--- shell.nix
+++ shell.nix
@@ -2,17 +2,19 @@
 
 pkgs.mkShell
 {
-    nativeBuildInputs = with pkgs; [
-        (php83.buildEnv {
-            extraConfig = ''
-                memory_limit = 6G
-                xdebug.mode=coverage
-            '';
+    nativeBuildInputs = with pkgs; let
+        php =         (php83.buildEnv {
+                          extraConfig = ''
+                              memory_limit = 6G
+                              xdebug.mode=coverage
+                          '';
 
-            extensions = ({ enabled, all }: enabled ++ (with all; [
-                xdebug
-            ]));
-        })
-        php83Packages.composer
+                          extensions = ({ enabled, all }: enabled ++ (with all; [
+                              xdebug
+                          ]));
+                      });
+     in [
+        php
+        php.packages.composer
     ];
 }
